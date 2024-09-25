0d95b1516ffebd9240e137ee8fd73c86a91403e2
camodogamingfan1 <751987@student.dusd.net>
2024-09-25 11:22:38 -0700

    Not done scripting
    i got to input the colors red and green colors for my script

---
+++ ScriptingProjectP4soto/Assets/Scripts/ColorChanger.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Windows.Media.Media3D;
+using UnityEngine;
+
+public class ColorChanger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+
+ GetComponent<Renderer>().material.color = Color.red; 
+    private void()   
+    {
+      if (Input.GetKeyDown(KeyCode.G))
+    }
+
+    GetComponent<Renderer>().Material.color = Color.green
+
+
+
+
+
+
+
+
+
+
